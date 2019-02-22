ba59161d45b78182592cfd5057f7d890b9425ec9
DillonRyan <46975298+DillonRyan@users.noreply.github.com>
2019-02-22 10:47:17 +0000

    Code for A+D

--- SortComparison.java
+++ SortComparison.java
@@ -18,7 +18,18 @@
      *
      */
     static double [] insertionSort (double a[]){
-
+    	double temp ;
+    	for(int i = 0; i < a.length; i++) {
+    		for(int j = i; j > 0; j--) {
+    			if(a[j] < a[j-1]) {
+    				temp = a[j];
+    				a[j] = a[j - 1];
+    				a[j-1] = temp;
+    			}
+    		}
+    	
+    	}
+    	return a;
         //todo: implement the sort
     }//end insertionsort
 
@@ -30,11 +41,49 @@
      *
      */
     static double [] quickSort (double a[]){
+    	int low = 0;
+    	int high = a.length-1;
+    	quickSortFunction(a,low, high);
+		return a;
 	
 		 //todo: implement the sort
 
     }//end quicksort
-
+    
+    private static void quickSortFunction(double a[], int low, int high) { //cng
+    	int midArray = low + (high - low) / 2;
+    	double pivot = a[midArray];
+    	int tempHigh = high;
+    	int templow = low;
+    	
+    	if(low >= high) {
+    		return;
+    	}
+    	while (templow <= tempHigh) {
+    		while(a[templow] < pivot) {
+    			low++;
+    		}
+    		while(a[tempHigh] > pivot) {
+    			tempHigh--;
+    		}
+    		if(low <= tempHigh) {
+    			double temp = a[templow];
+    			a[templow] = a[tempHigh];
+    			a[tempHigh] = temp;
+    			templow++;
+    			tempHigh--;
+    			
+    		}
+    	}
+    	if(low < tempHigh ) {
+    		quickSortFunction(a, low, tempHigh );
+    	}
+    	if(high > templow) {
+    		quickSortFunction(a, templow, high);
+    	}
+    }
+    	
+    
     /**
      * Sorts an array of doubles using Merge Sort.
      * This method is static, thus it can be called as SortComparison.sort(a)
@@ -51,10 +100,72 @@
      */
 
     static double[] mergeSortIterative (double a[]) {
-
+    	
+        int size;  
+        int left_start; 
+       
+        for (size = 1; size <= a.length-1;  size = 2*size) 
+        { 
+         
+            for (left_start = 0; left_start < a.length-1; left_start += 2*size) 
+            { 
+             
+                int mid = left_start + size - 1; 
+          
+                int right_end = Math.min(left_start + 2*size - 1, a.length-1); 
+                merge(a, left_start, mid, right_end); 
+            } 
+        } 
+        return a;
 		 //todo: implement the sort
 	
     }//end mergesortIterative
+    
+    static void merge(double a[], double l, double m, double r) 
+    { 
+        int i, j, k; 
+        double n1 = m - l + 1; 
+        double n2 = r - m; 
+        double L[] = new double[(int) n1]; 
+        double R[] = new double[(int) n2]; 
+      
+        for (i = 0; i < n1; i++) 
+            L[(int) i] = a[(int) (l + i)]; 
+        for (j = 0; j < n2; j++) 
+            R[(int)j] = a[(int) (m + 1+ j)]; 
+      
+        i = 0; 
+        j = 0; 
+        k = (int)l; 
+        while (i < n1 && j < n2) 
+        { 
+            if (L[i] <= R[j]) 
+            { 
+                a[k] = L[i]; 
+                i++; 
+            } 
+            else
+            { 
+                a[k] = R[j]; 
+                j++; 
+            } 
+            k++; 
+        } 
+      
+        while (i < n1) 
+        { 
+            a[k] = L[i]; 
+            i++; 
+            k++; 
+        } 
+
+        while (j < n2) 
+        { 
+            a[k] = R[j]; 
+            j++; 
+            k++; 
+        } 
+    } 
     
     
     
@@ -66,7 +177,54 @@
      * @return after the method returns, the array must be in ascending sorted order.
      */
     static double[] mergeSortRecursive (double a[]) {
-    	
+         if(a.length > 1) 
+         { 
+             int mid = a.length / 2; 
+             double[] left = new double[mid]; 
+             double[] right = new double[a.length - mid]; 
+             for(int i = 0; i < mid; i++) 
+             { 
+                 left[i] = a[i]; 
+             } 
+             for(int i = mid; i < a.length; i++) 
+             { 
+                 right[i - mid] = a[i]; 
+             } 
+             mergeSortRecursive(left); 
+             mergeSortRecursive(right); 
+   
+             int i = 0; 
+             int j = 0; 
+             int k = 0; 
+   
+             while(i < left.length && j < right.length) 
+             { 
+                 if(left[i] < right[j]) 
+                 { 
+                     a[k] = left[i]; 
+                     i++; 
+                 } 
+                 else
+                 { 
+                     a[k] = right[j]; 
+                     j++; 
+                 } 
+                 k++; 
+             } 
+             while(i < left.length) 
+             { 
+                 a[k] = left[i]; 
+                 i++; 
+                 k++; 
+             } 
+             while(j < right.length) 
+             { 
+                 a[k] = right[j]; 
+                 j++; 
+                 k++; 
+             } 
+         } 
+    	return a;
 
     	//todo: implement the sort
 	
@@ -81,6 +239,19 @@
      *
      */
     static double [] selectionSort (double a[]){
+    	int tmp;
+    	for (int i = 0; i < a.length-1; i++)
+    	{
+    	int min_idx = i;
+    	for (int j = i+1; j < a.length; j++)
+    	if (a[j] < a[min_idx])
+    	min_idx = j;
+    
+    	double temp = a[min_idx];
+    	a[min_idx] = a[i];
+    	a[i] = temp;
+    	}
+    	return a;
 
          //todo: implement the sort
 
--- SortComparisonTest.java
+++ SortComparisonTest.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
@@ -31,6 +32,165 @@ public class SortComparisonTest
     public void testEmpty()
     {
     }
+    @Test
+    public void testInsertionSort() {
+    	double a[] = {56.0}; // test with one value in Array
+    	double expectedArray[] = {56.0}; 
+    	double sortedArray[] = new double[a.length];
+    	sortedArray = SortComparison.insertionSort(a);
+    	assertArrayEquals(expectedArray, sortedArray, 0);
+    	
+    	double b[] = {91.0, 58.0,67.0}; // test with three values in Array
+    	double expectedArray1[] = {58.0, 67.0, 91.0};
+    	double sortedArray1[] = new double[b.length];
+    	sortedArray1 = SortComparison.insertionSort(b);
+    	assertArrayEquals(expectedArray1, sortedArray1, 0);
+    	
+    	double c[] = {5,-10,2}; // testing with both pos and neg numbers
+    	double expectedArray11[] = {-10,2,5};
+    	double sortedArray11[] = new double[c.length];
+    	sortedArray11 = SortComparison.insertionSort(c);
+    	assertArrayEquals(expectedArray11, sortedArray11, 0);
+    	
+    	
+    	double d[] = {-10, -70, -4}; // testing with only neg numbers
+    	double expectedArray111[] = {-70,-10,-4};
+    	double sortedArray111[] = new double[d.length];
+    	sortedArray111 = SortComparison.insertionSort(d);
+    	assertArrayEquals(expectedArray111, sortedArray111, 0);
+    	
+    	
+    }
+    
+    @Test
+    public void testQuickSort() 
+    {
+    	double a[] = {56.0}; // test with one value in Array
+    	double expectedArray[] = {56.0}; 
+    	double sortedArray[] = new double[a.length];
+    	sortedArray = SortComparison.quickSort(a);
+    	assertArrayEquals(expectedArray, sortedArray, 0);
+    	
+    	
+    	double b[] = {91.0, 58.0,67.0}; // test with three values in Array
+    	double expectedArray1[] = {58.0, 67.0, 91.0};
+    	double sortedArray1[] = new double[b.length];
+    	sortedArray1 = SortComparison.quickSort(b);
+    	assertArrayEquals(expectedArray1, sortedArray1, 0);
+    	
+    	double c[] = {5,-10,2}; // testing with both pos and neg numbers
+    	double expectedArray11[] = {-10,2,5};
+    	double sortedArray11[] = new double[c.length];
+    	sortedArray11 = SortComparison.quickSort(c);
+    	assertArrayEquals(expectedArray11, sortedArray11, 0);
+    	
+    	
+    	double d[] = {-10, -70, -4}; // testing with only neg numbers
+    	double expectedArray111[] = {-70,-10,-4};
+    	double sortedArray111[] = new double[d.length];
+    	sortedArray111 = SortComparison.quickSort(d);
+    	assertArrayEquals(expectedArray111, sortedArray111, 0);
+    	
+    	
+    }
+    
+    @Test
+    public void testselectionSort() 
+    {
+    	double a[] = {56.0}; // test with one value in Array
+    	double expectedArray[] = {56.0}; 
+    	double sortedArray[] = new double[a.length];
+    	sortedArray = SortComparison.selectionSort(a);
+    	assertArrayEquals(expectedArray, sortedArray, 0);
+    	
+    	
+    	double b[] = {91.0, 58.0,67.0}; // test with three values in Array
+    	double expectedArray1[] = {58.0, 67.0, 91.0};
+    	double sortedArray1[] = new double[b.length];
+    	sortedArray1 = SortComparison.selectionSort(b);
+    	assertArrayEquals(expectedArray1, sortedArray1, 0);
+    	
+    	double c[] = {5,-10,2}; // testing with both pos and neg numbers
+    	double expectedArray11[] = {-10,2,5};
+    	double sortedArray11[] = new double[c.length];
+    	sortedArray11 = SortComparison.selectionSort(c);
+    	assertArrayEquals(expectedArray11, sortedArray11, 0);
+    	
+    	
+    	double d[] = {-10, -70, -4}; // testing with only neg numbers
+    	double expectedArray111[] = {-70,-10,-4};
+    	double sortedArray111[] = new double[d.length];
+    	sortedArray111 = SortComparison.selectionSort(d);
+    	assertArrayEquals(expectedArray111, sortedArray111, 0);
+    	
+    	
+    }
+    
+    @Test
+    public void testMergesortrecursive() 
+    {
+    	double a[] = {56.0}; // test with one value in Array
+    	double expectedArray[] = {56.0}; 
+    	double sortedArray[] = new double[a.length];
+    	sortedArray = SortComparison.mergeSortRecursive(a);
+    	assertArrayEquals(expectedArray, sortedArray, 0);
+    	
+    	
+    	double b[] = {91.0, 58.0,67.0}; // test with three values in Array
+    	double expectedArray1[] = {58.0, 67.0, 91.0};
+    	double sortedArray1[] = new double[b.length];
+    	sortedArray1 = SortComparison.mergeSortRecursive(b);
+    	assertArrayEquals(expectedArray1, sortedArray1, 0);
+    	
+    	double c[] = {5,-10,2}; // testing with both pos and neg numbers
+    	double expectedArray11[] = {-10,2,5};
+    	double sortedArray11[] = new double[c.length];
+    	sortedArray11 = SortComparison.mergeSortRecursive(c);
+    	assertArrayEquals(expectedArray11, sortedArray11, 0);
+    	
+    	
+    	double d[] = {-10, -70, -4}; // testing with only neg numbers
+    	double expectedArray111[] = {-70,-10,-4};
+    	double sortedArray111[] = new double[d.length];
+    	sortedArray111 = SortComparison.mergeSortRecursive(d);
+    	assertArrayEquals(expectedArray111, sortedArray111, 0);
+    	
+    	
+    }
+    
+    @Test
+    public void testMergesortIterative() 
+    {
+    	double a[] = {56.0}; // test with one value in Array
+    	double expectedArray[] = {56.0}; 
+    	double sortedArray[] = new double[a.length];
+    	sortedArray = SortComparison.mergeSortIterative(a);
+    	assertArrayEquals(expectedArray, sortedArray, 0);
+    	
+    	
+    	double b[] = {91.0, 58.0,67.0}; // test with three values in Array
+    	double expectedArray1[] = {58.0, 67.0, 91.0};
+    	double sortedArray1[] = new double[b.length];
+    	sortedArray1 = SortComparison.mergeSortIterative(b);
+    	assertArrayEquals(expectedArray1, sortedArray1, 0);
+    	
+    	double c[] = {5,-10,2}; // testing with both pos and neg numbers
+    	double expectedArray11[] = {-10,2,5};
+    	double sortedArray11[] = new double[c.length];
+    	sortedArray11 = SortComparison.mergeSortIterative(c);
+    	assertArrayEquals(expectedArray11, sortedArray11, 0);
+    	
+    	
+    	double d[] = {-10, -70, -4}; // testing with only neg numbers
+    	double expectedArray111[] = {-70,-10,-4};
+    	double sortedArray111[] = new double[d.length];
+    	sortedArray111 = SortComparison.mergeSortIterative(d);
+    	assertArrayEquals(expectedArray111, sortedArray111, 0);
+    	
+    	
+    }
+
+
 
 
     // TODO: add more tests here. Each line of code and ech decision in Collinear.java should
